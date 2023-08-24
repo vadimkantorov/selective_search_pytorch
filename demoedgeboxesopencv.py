48df206add00a1429a95530cb708477883cc1f6a
Vadim Kantorov <vadimkantorov@gmail.com>
2023-08-24 23:40:13 +0200

    ...

--- demoedgeboxesopencv.py
+++ demoedgeboxesopencv.py
@@ -1,6 +1,7 @@
+import os
+import math
 import argparse
 import colorsys
-import math
 
 import numpy as np
 import matplotlib, matplotlib.pyplot as plt
@@ -9,10 +10,13 @@ import cv2
 parser = argparse.ArgumentParser()
 parser.add_argument('--sed-model-path', default = 'examples/model.yml.gz')
 parser.add_argument('--input-path', '-i', default = 'examples/astronaut.jpg')
-parser.add_argument('--output-path', '-o', default = 'demoedgeboxesopencv.png')
+parser.add_argument('--output-dir', '-o', default = './out')
 parser.add_argument('--topk', type = int, default = 32)
 parser.add_argument('--colorwheel-grid', type = int, default = 64)
 args = parser.parse_args()
+
+os.makedirs(args.output_dir, exist_ok = True)
+output_path = os.path.join(args.output_dir, os.path.basename(args.input_path))
 
 img_rgbhw3_1 = plt.imread(args.input_path).astype('float32') / 255.0
 
@@ -32,7 +36,7 @@ print('boxes_xywh:', boxes_xywh.shape, boxes_xywh.dtype, 'boxes_objn:', boxes_ob
 
 plt.figure(figsize = (6, 6))
 plt.subplot(221)
-plt.title('image')
+plt.title('#boxes = {topk}'.format(topk = args.topk))
 plt.imshow(img_rgbhw3_1)
 colormap = matplotlib.colormaps['jet']
 for (x, y, w, h), o in zip(boxes_xywh, boxes_objn):
@@ -47,14 +51,14 @@ plt.clim([0, 1])
 plt.axis('off')
 
 plt.subplot(223)
-plt.title('orimap hsv = h11')
+plt.title('orimap_hsv')
 xx, yy = np.meshgrid(np.linspace(-1.0, 1.0, args.colorwheel_grid), np.linspace(-1.0, 1.0, args.colorwheel_grid))
 H = (np.arctan2(xx, yy) + math.pi) / (math.pi * 2)
 S = np.sqrt(xx ** 2 + yy ** 2)
 colorwheel = matplotlib.colors.hsv_to_rgb(np.dstack([H, S, np.where((S < 1) & (S > 0.8), 1, 0) ]))
-orimap_vis = matplotlib.colors.hsv_to_rgb( np.dstack([orimap / math.pi, np.full_like(orimap, 1.0), np.full_like(orimap, 1.0)]) ) * edges[..., None] + 1 * (1 - edges[..., None])
-orimap_vis[:colorwheel.shape[0], :colorwheel.shape[1]] = colorwheel
-plt.imshow(orimap_vis)
+orimap_hsv = matplotlib.colors.hsv_to_rgb( np.dstack([orimap / math.pi, np.full_like(orimap, 1.0), np.full_like(orimap, 1.0)]) ) * edges[..., None] + 1 * (1 - edges[..., None])
+orimap_hsv[:colorwheel.shape[0], :colorwheel.shape[1]] = colorwheel
+plt.imshow(orimap_hsv)
 plt.axis('off')
 
 plt.subplot(224)
@@ -64,7 +68,7 @@ plt.colorbar()
 plt.clim([0, 1])
 plt.axis('off')
 
-plt.savefig(args.output_path, dpi = 300)
+plt.savefig(output_path, dpi = 300)
 
-print(args.output_path)
+print(output_path)
 
