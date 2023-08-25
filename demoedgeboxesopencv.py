5ccfdb5db05d3afb0bfbe025db36ee2308412f30
Vadim Kantorov <vadimkantorov@gmail.com>
2023-08-25 04:19:31 +0200

    ...

--- demoedgeboxesopencv.py
+++ demoedgeboxesopencv.py
@@ -7,12 +7,32 @@ import numpy as np
 import matplotlib, matplotlib.pyplot as plt
 import cv2
 
-def computeOrientation(src, gradientNormalizationRadius = 4, eps = 1e-5):
-    assert src.dtype == np.float32 and src.ndim == 2
+
+########################################
+########################################
+########################################
+########################################
+
+def edgesSpatialGrads(img, mode = 'canny'):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_gray_blur = cv2.GaussianBlur(img_gray, (3,3), 0)
+    
+    if mode == 'canny':
+        return cv2.Canny(image = img_gray_blur, threshold1 = 100, threshold2 = 200)
+    
+    if mode == 'sobel':
+        grad_x = cv2.Sobel (src_gray_blur, ddepth = -1, dx = 1, dy = 0, ksize = 3, scale = scale, delta = delta, borderType = BORDER_DEFAULT )
+        grad_y = cv2.Sobel (src_gray_blur, ddepth = -1, dx = 0, dy = 1, ksize = 3, scale = scale, delta = delta, borderType = BORDER_DEFAULT )
+        return (np.abs(grad_x) + np.abs(grady_y)) / 2
+        
+    return edges
+
+def computeOrientation(E, gradientNormalizationRadius = 4, eps = 1e-5):
+    assert E.dtype == np.float32 and E.ndim == 2
     
     nrml = (gradientNormalizationRadius + 1.0) ** 2
     kernelXY = np.array([(i + 1) / nrml for i in range(1 + gradientNormalizationRadius)] + [(i + 1) / nrml for i in range(0 + gradientNormalizationRadius)][::-1])
-    E_conv = cv2.sepFilter2D(src, -1, kernelXY, kernelXY)
+    E_conv = cv2.sepFilter2D(E, -1, kernelXY, kernelXY)
     
     Oxx = cv2.Sobel(E_conv, -1, 2, 0)
     Oxy = cv2.Sobel(E_conv, -1, 1, 1)
@@ -28,44 +48,37 @@ def edgesNms(E, O, r = 2):
     assert E.dtype == np.float32 and E.ndim == 2
     assert O.dtype == np.float32 and O.ndim == 2
     
-    res = np.zeros_like(E)
-    Ocos = np.cos(O)
-    Osin = np.sin(O)
-    
-    Ocos_t = Ocos.T
-    Osin_t = Osin.T
-    O_t = O.T
-    E_t = E.T
+    res = E.copy()
+    Ocos, Osin = np.cos(O), np.sin(O)
 
-    for x in range(E.shape[1]):
-        for y in range(E.shape[0]):
-            e = res[y, x] = E_t[x, y]
-            (coso, sino) = (Ocos_t[x, y], Osin_t[x, y])
-            
+    for y in range(E.shape[0]):
+        for x in range(E.shape[1]):
+            e = E[y, x]
             if e == 0:
                 continue
 
+            (coso, sino) = (Ocos[y, x], Osin[y, x])
             for d in sorted(set(range(-r, r + 1)) - set([0])):
-                xdcos = max(0, min(x + d * coso, E.shape[1] - 1.001))
                 ydsin = max(0, min(y + d * sino, E.shape[0] - 1.001))
+                xdcos = max(0, min(x + d * coso, E.shape[1] - 1.001))
                 
                 # bilinear interpolation
                 (x0, y0) = (int(xdcos), int(ydsin))
                 (x1, y1) = (x0 + 1, y0 + 1)
                 (dx0, dy0) = (xdcos - x0, ydsin - y0)
                 (dx1, dy1) = (1 - dx0, 1 - dy0)
-                e0 =  E_t[x0, y0] * dx1 * dy1 + E_t[x1, y0] * dx0 * dy1 + E_t[x0, y1] * dx1 * dy0 + E_t[x1, y1] * dx0 * dy0
+                ed =  E[y0, x0] * dx1 * dy1 + E[y0, x1] * dx0 * dy1 + E[y1, x0] * dx1 * dy0 + E[y1, x1] * dx0 * dy0
 
-                if e == 0 or e < e0:
+                if e < ed:
                     res[y, x] = 0
                     break
 
     return res
 
-#######################################3
-#######################################3
-#######################################3
-#######################################3
+########################################
+########################################
+########################################
+########################################
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--sed-model-path', default = 'examples/model.yml.gz')
@@ -83,11 +96,9 @@ img_rgbhw3_1 = plt.imread(args.input_path).astype('float32') / 255.0
 sed = cv2.ximgproc.createStructuredEdgeDetection(args.sed_model_path)
 edges  = sed.detectEdges(img_rgbhw3_1)
 orimap = sed.computeOrientation(edges)
-orimap_ = computeOrientation(edges)
 edges_nms  = sed.edgesNms(edges, orimap)
-edges_nms_  = edgesNms(edges, orimap)
-breakpoint()
-
+#orimap_ = computeOrientation(edges)
+#edges_nms_  = edgesNms(edges, orimap)
 
 edge_boxes = cv2.ximgproc.createEdgeBoxes()
 edge_boxes.setMaxBoxes(args.topk)
@@ -135,22 +146,3 @@ plt.axis('off')
 plt.savefig(output_path, dpi = 300)
 
 print(output_path)
-
-#img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#img_blur = cv2.GaussianBlur(img_gray, (3,3), 0)
-#sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5) # Sobel Edge Detection on the X axis
-#sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5) # Sobel Edge Detection on the Y axis
-#sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # Combined X and Y Sobel Edge Detection
-#edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200) # Canny Edge Detection
-  
-#GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT );
-#cvtColor( src, src_gray, COLOR_BGR2GRAY );
-#Mat grad_x, grad_y;
-#Mat abs_grad_x, abs_grad_y;
-#//Scharr( src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
-#Sobel( src_gray, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
-#//Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
-#Sobel( src_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
-#convertScaleAbs( grad_x, abs_grad_x );
-#convertScaleAbs( grad_y, abs_grad_y );
-#addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
